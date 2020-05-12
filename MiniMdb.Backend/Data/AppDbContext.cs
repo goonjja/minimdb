e8640daaaeeb8f9ef66f8e8611a1aab01f07cb24
Sergei Vedernikov <vedernikov.s@gmail.com>
2020-05-12 09:27:26 +0200

    Add EF core

---
+++ MiniMdb.Backend/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MiniMdb.Backend.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.UseIdentityColumns();
+        }
+    }
+}
